975915a9810092044ac48f33ce725d4e2db60498
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-06-11 18:49:43 +0200

    more work done

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,12 @@
 class TestsController < ApplicationController
   def show
     @test = Test.find(params[:id])
+    @before_last_result, @last_result = @test.results.order(:created_at).last(2)
+
+    # si on a que 1 result, il se retouve en before last alors on le remet en last
+    unless @last_result
+      @last_result        = @before_last_result
+      @before_last_result = nil
+    end
   end
 end
