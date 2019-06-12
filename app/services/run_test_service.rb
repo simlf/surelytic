d6b6878fca17c99893e234d9be3ad832f6946b69
aristide riou <aristide.riou@gmail.com>
2019-06-12 12:22:03 +0200

    kept on GA logic implementation

--- app/services/run_test_service.rb
+++ app/services/run_test_service.rb
@@ -4,40 +4,71 @@ class RunTestService
   end
 
   def call
-    @test = Test.find('blabla')
+    @test = Test.find(params)
+    @result = Result.new
 
-    # gestion de la date
-    # si test frequency = daily ==> start date = end date = yesterday
-    # si test weekly = start date ...
-    # si test monthly
+    # default values
+    api_start_date = 'yesterday'
+    api_end_date = 'yesterday'
+    api_metric = 'ga:pageviews'
+    api_dimension = 'ga:dimension15'
+    api_condition_regex = ''
+    api_view_id = @test.project.ga_view_id
 
-    # si test type =
+    if @test.frequency == 'weekly'
+      api_start_date = '7daysAgo'
+    elsif @test.frequency == 'monthly'
+      api_start_date = '30daysAgo'
+    end
+
+    if @test.ga_report_type == 'custom dimension'
+      api_metric = @test.cd_scope
+      api_dimension = `ga:dimension#{@test.cd_index}`
+      api_condition_regex = `ga:dimension#{@test.cd_index}=~#{@test.cd_regex}`
+
+    elsif @test.ga_report_type == 'event'
+      api_metric = 'ga:totalEvents'
+      api_dimension = 'ga:eventCategory'
+      api_condition_regex = `ga:eventCategory=~#{@test.event_category_regex}`
+
+    elsif @test.ga_report_type == 'enhanced e-commerce'
+      api_metric = 'ga:transactions'
+      api_dimension = 'ga:productName'
+      api_condition_regex = `ga:productName=~#{@test.eec_product_name_regex}`
+
+    elsif @test.ga_report_type == 'goals'
+      api_metric = `ga:goal#{@test.goal_index}completions`
+    end
 
     service = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
-    # Create service account credentials
     credentials = Google::Auth::ServiceAccountCredentials.make_creds(
       json_key_io: File.open(Rails.root.join('service_account_cred.json')),
       scope: 'https://www.googleapis.com/auth/analytics.readonly'
     )
     service.authorization = credentials
     google_client = service
-    # Set the date range - this is always required for report requests
     date_range = Google::Apis::AnalyticsreportingV4::DateRange.new(
-      start_date: '2019-06-01', end_date: '2019-06-10'
+      start_date: api_start_date, end_date: api_end_date
     )
-    # Set the metric
     metric = Google::Apis::AnalyticsreportingV4::Metric.new(
-      expression: 'ga:users'
+      expression: api_metric
     )
-    # Set the dimension
     dimension = Google::Apis::AnalyticsreportingV4::Dimension.new(
-      name: 'ga:browser'
+      name: api_dimension
     )
-    # Build report request and add country filter
+
     report_request = Google::Apis::AnalyticsreportingV4::ReportRequest.new(
+      view_id: api_view_id,
+      sampling_level: 'DEFAULT',
+      date_ranges: [date_range],
+      metrics: [metric],
+      dimensions: [dimension]
+    )
+
+    report_request_with_regex = Google::Apis::AnalyticsreportingV4::ReportRequest.new(
       view_id: '196537952',
       sampling_level: 'DEFAULT',
-      filters_expression: 'ga:country==France',
+      filters_expression: api_condition_regex,
       date_ranges: [date_range],
       metrics: [metric],
       dimensions: [dimension]
@@ -47,9 +78,16 @@ class RunTestService
     request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new({
       report_requests: [report_request]
     })
+    request_with_regex = Google::Apis::AnalyticsreportingV4alyticsreportingV4::GetReportsRequest.new({
+      report_requests: [report_request_with_regex]
+    })
 
-    # API call
+    # API calls
     response = google_client.batch_get_reports(request)
+    response_with_regex = google_client.batch_get_reports(request_with_regex)
+
+    # Do stuff with both responses
+    # ga_api_results = @response.reports[0].data.rows
   end
 end
 
