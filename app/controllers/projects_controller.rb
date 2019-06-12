8f03f99207d3da95115559f3162a7dc4f7d57c5d
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-06-12 14:49:50 +0200

    start set up C

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,23 @@
 class ProjectsController < ApplicationController
   def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+    @project.organization = current_user.organization
+
+    @project.save
+    redirect_to project_path(@project)
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -9,4 +9,39 @@ class TestsController < ApplicationController
       @before_last_result = nil
     end
   end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    project = current_user.projects.find(params[:project_id])
+    @test = Test.new(test_params)
+    @test.project = project
+    @test.user = current_user
+
+    @test.save
+    redirect_to project_path(project)
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(
+      :name,
+      :frequency,
+      :ga_report_type,
+      :cd_index,
+      :cd_regex,
+      :cd_scope,
+      :eec_product_name_regex,
+      :eec_product_id_regex,
+      :eec_product_category_regex,
+      :eec_product_brand_regex,
+      :event_category_regex,
+      :event_action_regex,
+      :event_label_regex,
+      :goal_index
+    )
+  end
 end
