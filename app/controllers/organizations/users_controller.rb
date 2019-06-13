e266ada1d0dd74ea901d9fb4680c7089f2322aa4
simlf <lefournsimon@gmail.com>
2019-06-13 17:00:21 +0200

    new user

--- app/controllers/organizations/users_controller.rb
+++ app/controllers/organizations/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Organizations::UsersController < ApplicationController
 
   def new
     @user = User.new
@@ -9,16 +9,16 @@ class UsersController < ApplicationController
     @user.organization = current_user.organization
 
     if @user.save
-      redirect_to organization_path(@organization)
+      redirect_to organization_path
     else
-      render new
+      render :new
     end
   end
 
   private
 
   def user_params
-    params.require(:organization).permit(:email, :password)
+    params.require(:user).permit(:email, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :organization, only: [:show] # opt. :edit, :update
 
-  resources :users, only: [:new, :create]
-
+  namespace :organization, module: :organizations do
+    resources :users, only: [:new, :create]
+  end
 
   resources :projects, only: [:show, :new, :create] do # opt. :index, :edit, :update // :destroy
     resources :tests, only: [:show, :new, :create, :edit, :update] # opt. // :destroy
