a7f0c51dcb32fafb26cef442afce14b329fdef15
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-06-14 12:41:06 +0200

    fix redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  private
+
+  def after_sign_in_path_for(user)
+    organization_path
+  end
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
