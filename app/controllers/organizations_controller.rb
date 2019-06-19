985ceaba2f1cca78f87ce1c459e12e44dc61f1f9
aristide riou <aristide.riou@gmail.com>
2019-06-19 15:47:06 +0200

    chart js on orga page

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,6 +3,17 @@ class OrganizationsController < ApplicationController
     @organization = current_user.organization
     @users = current_user.organization.users
     @projects = current_user.projects
-  end
 
+    @chart_data_labels = []
+    @chart_data_incorrect = []
+
+    @projects.each do |project|
+      incorrect_val = 0
+      @chart_data_labels.push(project.name)
+      project.tests.each do |test|
+        incorrect_val += test.results.last.number_incorrect_values unless test.results.last.nil?
+      end
+      @chart_data_incorrect.push(incorrect_val)
+    end
+  end
 end
