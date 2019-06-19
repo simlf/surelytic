3f4e03541bd2a53bd3826aa957a558d716fff7bc
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-06-19 10:46:13 +0200

    first dashtry

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,6 +2,7 @@ class OrganizationsController < ApplicationController
   def show
     @organization = current_user.organization
     @users = current_user.organization.users
-    @projects = current_user
+    @projects = current_user.projects
   end
+
 end
