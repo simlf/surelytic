6d22689ea90824d049108acbbd223cd2ccbe4137
aristide riou <aristide.riou@gmail.com>
2019-06-20 11:35:33 +0200

    wip on css refacto for landing

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,18 +3,5 @@ class OrganizationsController < ApplicationController
     @organization = current_user.organization
     @users = current_user.organization.users
     @projects = current_user.projects
-
-    # Chart
-    @chart_data_labels = []
-    @chart_data_incorrect = []
-    @projects.each do |project|
-      incorrect_val = 0
-      @chart_data_labels.push(project.name)
-      project.tests.each do |test|
-        incorrect_val += test.results.last.number_incorrect_values unless test.results.last.nil?
-      end
-      @chart_data_incorrect.push(incorrect_val)
-    end
-    # Chart
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -67,45 +67,65 @@ if (document.getElementById('chart-double-bar')){
   };
 }
 
-// Chart JS setup - Simple bars
+// Chart JS setup - Simple line
 
-if (document.getElementById('chart-simple-bar')){
-  const chartLabels = document.getElementById('chart-simple-bar').dataset.chartLabels;
-  const chartIncorrectValues = document.getElementById('chart-simple-bar').dataset.chartErrors;
-
-  window.onload = function() {
-    const ctx = document.getElementById('chart-simple-bar').getContext('2d');
-    window.myBar = new Chart(ctx, {
-      type: 'bar',
+if (document.getElementById('chart-line')){
+ var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var config = {
+      type: 'line',
       data: {
-        labels : JSON.parse(chartLabels),
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
-        label: '',
-        data : JSON.parse(chartIncorrectValues),
-        backgroundColor: [
-          'red','red','red','red','red','red'
-        ],
-        borderWidth: 1
+          label: 'My First dataset',
+          backgroundColor: 'white',
+          borderColor: 'blue',
+          data: [12,11,10,9,8,7],
+          fill: false,
+        }, {
+          label: 'My Second dataset',
+          fill: false,
+          backgroundColor: 'white',
+          borderColor: 'blue',
+          data: [8,6,4,12,7,5],
         }]
       },
       options: {
-        responsive: false,
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Chart.js Line Chart'
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
         scales: {
           xAxes: [{
-            ticks: {
-              maxRotation: 90,
-              minRotation: 80
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Month'
             }
           }],
           yAxes: [{
-            ticks: {
-              beginAtZero: true
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Value'
             }
           }]
         }
       }
-    });
-  };
+    };
+
+    window.onload = function() {
+      var ctx = document.getElementById('chart-line').getContext('2d');
+      window.myLine = new Chart(ctx, config);
+    };
 }
 
 
